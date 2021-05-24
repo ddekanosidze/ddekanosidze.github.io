175a520c6939dc85c864537a6cf14d2709aa9e94
ddekanosidze <44446303+ddekanosidze@users.noreply.github.com>
2021-05-24 16:47:36 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".test1").addEventListener("click",function(){
+    document.querySelector("body").style.background = "green"
+})
+document.querySelector(".test2").addEventListener("click",function(){
+    document.querySelector("body").style.background = "red"
+})
